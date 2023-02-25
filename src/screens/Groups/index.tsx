08d3a37a4254bdd1c0b7ed67fffb4001f5a1fd4e
Bruno Ferreira <brunoferreirapedraca@gmail.com>
2023-02-25 17:32:13 -0300

    CSS-in-JS - Utilizando tema

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,8 @@
-import { Container, Title} from './styles'
+import { Container} from './styles'
 
 export function Groups() {
   return (
     <Container>
-      <Title>App Ignite Teams</Title>
     </Container>
   );
 }
