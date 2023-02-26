f338d7227fc2c2d4c7d51b6699e7d9e16582160f
Bruno Ferreira <brunoferreirapedraca@gmail.com>
2023-02-26 15:37:24 -0300

    CSS-in-JS - Criando a interface Players

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import theme from './src/theme'
 
 import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 
-import { Groups } from '@screens/Groups';
+import { Players } from '@screens/Players';
 import { Loading } from '@components/Loading'
 import { NewGroup } from '@screens/NewGroup';
 
@@ -20,7 +20,7 @@ export default function App() {
           backgroundColor='transparent'
           translucent
         />
-        { fontsLoaded ? <NewGroup/> : <Loading/>}
+        { fontsLoaded ? <Players/> : <Loading/>}
     </ThemeProvider>
   );
 }
